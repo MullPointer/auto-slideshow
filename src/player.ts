eef2b51f3808ee5cbef8f2d69797cc88036f645a
MullPointer <127130989+MullPointer@users.noreply.github.com>
2024-05-01 18:14:51 +0100

    Slideshow function with speech in player

--- src/player.ts
+++ src/player.ts
@@ -6,10 +6,17 @@ import { preloadImage } from "./utility.js";
 
 
 
+const slideTimeoutIfNoText = 5000;
+const slideLingerAfterSpeaking = 500;
 
 const params = new URLSearchParams(window.location.search);
 const slides:SlideProperties[] = new Array();
+let currentUtterance:SpeechSynthesisUtterance = null;
 let currentSlide = -1;
+
+
+
+
 
 if (params.has('data')) {
     const data = params.get('data');
@@ -25,10 +32,98 @@ for (const slide of slides) {
     //TODO - wait to load some images if many slides
 }
 
+function showPlayer() {
+    document.getElementById('intro').classList.add('hidden');
+    document.getElementById('player').classList.remove('hidden');
+}
 
-document.getElementById('ctrl-start-slideshow').addEventListener('click', (
-    event: PointerEvent & { target: HTMLInputElement }
-    ) => {
+function showIntro() {
+    document.getElementById('player').classList.add('hidden');
+    document.getElementById('intro').classList.remove('hidden');
+}
+
+
+async function speak(text:string): Promise<void> {
+    return new Promise((resolve,reject) => {
+        const synth = window.speechSynthesis;
+        if (currentUtterance || synth.speaking || synth.pending) {
+            //any speaking should have finished before we speak again so this is handling unexpected event
+            console.warn('New speech synthesis started before previous complete');
+            synth.cancel();
+            currentUtterance = null;
+        }
+
+        console.log('speaking: ', text);
+        currentUtterance = new SpeechSynthesisUtterance(text);
+        currentUtterance.onerror = (e) => {
+            console.error('error in speech synthesis: ', e.error);
+            currentUtterance = null;
+            reject();
+        };
+        //TODO - add a timeout on speaking pending too long
+        currentUtterance.onend = () => {
+            currentUtterance = null;
+            resolve();
+        };
+        synth.speak(currentUtterance);
+    });
+}
+
+function showImage(url:string) {
+    const img = document.getElementById('displayImg') as HTMLImageElement;
+    if (!url || url === '') {
+        img.classList.add('invisible');
+    }
+    else
+    {
+        img.classList.remove('invisible');
+        img.src = url;
+    }
+}
+
+
+async function advanceSlide() {
+    if (slides.length <= 0) {
+        return;
+    }
+
+    currentSlide++;
+
+    if (currentSlide == 0) {
+        showPlayer();
+        console.log('slideshow begins on slide ', currentSlide, slides[currentSlide]);
+    }
+    else if (currentSlide >= slides.length) {
+        currentSlide = -1;
+        showIntro();
+        console.log('slideshow ends');
+    }
+    else {
+        console.log('slide advances to ', currentSlide, slides[currentSlide]);
+    }
+
+    if (currentSlide >= 0) {
+        showImage(slides[currentSlide].imgURL);
+
+        if (slides[currentSlide].text.trim() === '') {
+            if (slides[currentSlide].imgURL === '') {
+                setTimeout(advanceSlide); //advance immediately as showing nothing
+            }
+            else {
+                setTimeout(advanceSlide, slideTimeoutIfNoText);
+            }
+        }
+        else {
+            await speak(slides[currentSlide].text)
+                .then(() => setTimeout(advanceSlide, slideLingerAfterSpeaking), 
+                    () => setTimeout(advanceSlide, slideTimeoutIfNoText));
+                    //waiting full timeout may be a while if error occurred at end of speaking, but most likely happens at start
+        }
+    }
     
+}
 
+
+document.getElementById('ctrl-start-slideshow').addEventListener('click', async () => {
+    advanceSlide();
 });
