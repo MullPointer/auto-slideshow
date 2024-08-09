f5016d91de20f32167de7a4a453990befb1bc16f
MullPointer <127130989+MullPointer@users.noreply.github.com>
2024-08-09 17:58:37 +0100

    Remove fiveserver config

--- fiveserver.config.js
+++
@@ -1,9 +0,0 @@
-// fiveserver.config.js
-module.exports = {
-    highlight: false, // enable highlight feature
-    injectBody: false, // enable instant update
-    remoteLogs: false, // enable remoteLogs
-    remoteLogs: "yellow", // enable remoteLogs and use the color yellow
-    injectCss: false, // disable injecting css
-    navigate: false, // enable auto-navigation
-  };
